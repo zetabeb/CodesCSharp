1a276a745d2ae7c7752f227448cb85e98019a227
Zamir <59321983+zetabeb@users.noreply.github.com>
2021-09-27 21:10:06 -0500

    Solución error administrador de paquetes NuGet

--- HallarDatos.cs
+++ HallarDatos.cs
@@ -1,3 +1,8 @@
+/*
+* Cuando el administrados de paquetes NuGet de Visual Studio tiene fallas como por ejemplo que no carga, 
+* correr esto en la consola de administrador de paquetes de Visual Studio:
+* [Net.ServicePointManager]::SecurityProtocol=[Net.ServicePointManager]::SecurityProtocol-bOR [Net.SecurityProtocolType]::Tls12
+*/
 public static string BuscarDireccionIP()
 {
     IPAddress DirIP=null;
