0be1902583d2e115ac18b32f664e1309f60055b2
Zamir <59321983+zetabeb@users.noreply.github.com>
2021-11-11 00:30:31 -0500

    Update HallarDatos.cs

--- HallarDatos.cs
+++ HallarDatos.cs
@@ -1,5 +1,5 @@
 /*
-* Cuando el administrados de paquetes NuGet de Visual Studio tiene fallas como por ejemplo que no carga, 
+* Cuando el administrador de paquetes NuGet de Visual Studio tiene fallas como por ejemplo que no carga, 
 * correr esto en la consola de administrador de paquetes de Visual Studio:
 * [Net.ServicePointManager]::SecurityProtocol=[Net.ServicePointManager]::SecurityProtocol-bOR [Net.SecurityProtocolType]::Tls12
 */
